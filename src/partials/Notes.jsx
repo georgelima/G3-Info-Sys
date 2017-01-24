29c18523da34a6fe298865fe82ff8b57ea7ca819
George Lima <georgelima11@hotmail.com>
2017-01-24 00:40:41 -0300

    inicio partial notes

--- src/partials/Notes.jsx
+++ src/partials/Notes.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Notes = () => (
-  <h1>Notes...</h1>
-);
+import Table from '../components/Table';
+
+class Notes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      statusFetchNotes: true
+    }
+  }
+
+  render() {
+    return (
+      <div className="has-text-centered">
+        <h1 className="title is-3">Notas Cadastradas</h1>
+        <Table 
+          body={[]}
+          header={['Cliente', 'Valor', 'Info', 'Remover']}
+          isLoaded={ this.state.statusFetchNotes }       
+        />  
+      </div>
+    )
+  }
+}
 
 export default Notes;
